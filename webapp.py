5aec287a8be9a2dc30c997c652cd9f67a791578b
ConnellyR <madekath@icloud.com>
2019-03-08 10:22:36 -0800

    test

--- webapp.py
+++ webapp.py
@@ -5,7 +5,10 @@ from flask import session
 
 app = Flask(__name__)
 
-#app.secret_key=os.environ["SECRET_KEY"];   
+app.secret_key=os.environ["SECRET_KEY"];   
+pg1data = 0
+pg2data = ""
+
 
 @app.route('/', methods=["POST","GET"])
 def renderHome():   
@@ -14,21 +17,22 @@ def renderHome():
 
 @app.route('/page1',methods=["POST","GET"])
 def renderPage1():
-   
     return render_template("page1.html")
     
 @app.route('/page2',methods=["POST","GET"])
-def renderCheckout():
-   # if "info" not in session:
-        #return redirect(url_for("renderPage1"))
+def renderPage2():
+    session["pg1data"] = request.form["pages"]
+    if "pg1data" not in session:
+        return redirect(url_for("renderPage1"))
     return render_template("page2.html")
 
 @app.route('/page3',methods=["POST","GET"])
 def renderPage3():
-   # if "info" not in session:
-      #  return redirect(url_for("renderPage3"))
-    #items = session["itemsincart"]
-    #session["itemsincart"] = 0
+    session["pg2data"] = request.form["genre"]
+    if "pg2data" not in session:
+        return redirect(url_for("renderPage2"))
+
+    
     return render_template("page3.html")
     
 if __name__=="__main__":
