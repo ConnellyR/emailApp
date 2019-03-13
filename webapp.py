d13422ad8b9d134150c6b7dcfbfec665e9d6acb6
ConnellyR <madekath@icloud.com>
2019-03-13 12:42:32 -0700

    mail

--- webapp.py
+++ webapp.py
@@ -5,7 +5,7 @@ from flask import session
 from flask_mail import Mail,Message
 
 app = Flask(__name__)
-mail = Mail(app)
+
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
@@ -14,7 +14,7 @@ app.config['MAIL_PASSWORD'] = os.environ["Pass"]
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
-
+mail = Mail(app)
 
 
 
@@ -29,28 +29,34 @@ def renderHome():
 
 @app.route('/page1',methods=["POST","GET"])
 def renderPage1():
+#get date/time
     return render_template("page1.html")
     
 @app.route('/page2',methods=["POST","GET"])
 def renderPage2():
     session["pg1data"] = request.form["pages"]
+    session["pg3data"] = request.form["name"]
     if "pg1data" not in session:
         return redirect(url_for("renderPage1"))
+    
     return render_template("page2.html")
 
 @app.route('/page3',methods=["POST","GET"])
 def renderPage3():
     session["pg2data"] = request.form["genre"]
+    session["readEveryDay"] = request.form["readDaily"]
     if "pg2data" not in session:
         return redirect(url_for("renderPage2"))
         
     print(session["pg1data"])
     print(session["pg2data"])
     
+    attachData="pg3data,pg1data,readEveryDay,pg2data\n\"%s\",\"%s\",\"%s\",\"%s\""%(session["pg3data"],session["pg1data"],session["readEveryDay"],session["pg2data"])
+    
     msg = Message('Hello', sender = 'connellyrogers@gmail.com', recipients = ['connellyrogers@gmail.com'])
-    msg.attach("data.csv","text/csv","pg1data,pg2data\n\"%s,%s"%(session["pg1data"],session["pg2data"]))
+    msg.attach("data.csv","text/csv",attachData)
    
-   mail.send(msg)
+    mail.send(msg)
     
     return render_template("page3.html")
     # put info in csv then email out
