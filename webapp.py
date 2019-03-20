abdcbf011f73cdc331215ac44123c89eac451993
ConnellyR <42652898+ConnellyR@users.noreply.github.com>
2019-03-20 12:47:50 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-import os
+import os, datetime
 from flask import Flask, url_for, render_template, request
 from flask import redirect
 from flask import session
@@ -29,20 +29,25 @@ def renderHome():
 
 @app.route('/page1',methods=["POST","GET"])
 def renderPage1():
-#get date/time
-    return render_template("page1.html")
+    var=datetime.datetime.now().strftime("%m, month, %d, day, %H, hour, %M, minute ")
+    #session["pg4data"] = var
+    return render_template("page1.html",VAR=var)
     
 @app.route('/page2',methods=["POST","GET"])
 def renderPage2():
-    session["pg1data"] = request.form["pages"]
-    session["pg3data"] = request.form["name"]
-    if "pg1data" not in session:
-        return redirect(url_for("renderPage1"))
+    if "back" not in request.args:
+        session["pg1data"] = request.form["pages"]
+        session["pg3data"] = request.form["name"]
+        session["pg4data"] = request.form["date"]
+        if "pg1data" not in session:
+            return redirect(url_for("renderPage1"))
     
     return render_template("page2.html")
 
 @app.route('/page3',methods=["POST","GET"])
 def renderPage3():
+    #if "back" in request.args:
+     
     session["pg2data"] = request.form["genre"]
     session["readEveryDay"] = request.form["readDaily"]
     if "pg2data" not in session:
@@ -50,12 +55,11 @@ def renderPage3():
         
     print(session["pg1data"])
     print(session["pg2data"])
-    
-    attachData="pg3data,pg1data,readEveryDay,pg2data\n\"%s\",\"%s\",\"%s\",\"%s\""%(session["pg3data"],session["pg1data"],session["readEveryDay"],session["pg2data"])
-    
+    attachData="pg3data,pg1data,pg4data,readEveryDay,pg2data\n\"%s\",\"%s\",\"%s\",\"%s\",\%s\"%(session["pg3data"],session["pg1data"],session["pg4data"],session["readEveryDay"],session["pg2data"])
+        
     msg = Message('Hello', sender = 'connellyrogers@gmail.com', recipients = ['connellyrogers@gmail.com'])
     msg.attach("data.csv","text/csv",attachData)
-   
+       
     mail.send(msg)
     
     return render_template("page3.html")
